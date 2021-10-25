842b5f5633c940d98a372a9c3789eb9710e0eb51
David Mwendwa <klatzdavie19@gmail.com>
2021-10-25 13:57:19 +0300

    refactor: move expenses lists into expenses list

---
+++ src/components/Expenses.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ExpenseItem from './ExpenseItem';
+import './Expenses.css';
+
+function Expenses({ expenses }) {
+  return (
+    <div className='expenses'>
+      {expenses.map((expense) => (
+        <ExpenseItem key={expense.id} {...expense} />
+      ))}
+    </div>
+  );
+}
+
+export default Expenses;
