38de7fb97170bd498fe45da8436c111910b4a515
David Mwendwa <klatzdavie19@gmail.com>
2021-10-26 08:39:38 +0300

    feat: handle form submission

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -10,19 +10,35 @@ const ExpenseForm = () => {
 
   const titleChangeHandler = (e) => {
     e.preventDefault();
-    setUserInput({...userInput, enteredTitle: e.target.value})
+    setUserInput(prevState => {
+      return { ...prevState, enteredTitle: e.target.value };
+    })
   };
   const amountChangeHandler = (e) => {
     e.preventDefault();
-    setUserInput({ ...userInput, enteredAmount: e.target.value });
+    setUserInput((prevState) => {
+      return { ...prevState, enteredAmount: e.target.value };
+    });
   };
   const dateChangeHandler = (e) => {
     e.preventDefault();
-    setUserInput({ ...userInput, enteredDate: e.target.value });
+    setUserInput((prevState) => {
+      return { ...prevState, enteredDate: e.target.value };
+    });
   };
 
+  const submitHandler = (e) => {
+    e.preventDefault()
+    const expenseData = {
+      title: userInput.enteredTitle,
+      amount: userInput.enteredAmount,
+      date: userInput.enteredDate,
+    };
+    console.log(expenseData)
+  }
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label>Title</label>
